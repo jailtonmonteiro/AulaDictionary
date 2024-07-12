1f52810beda5d79163ceebe1b55107f4abf6e543
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-07-11 21:20:29 -0300

    Aula sobre Directory.

---
+++ AulaDictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AulaDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "99887766";
+
+            Console.WriteLine(cookies["email"]);
+
+            cookies.Remove("email");
+
+            if (cookies.ContainsKey("email"))
+            {
+                Console.WriteLine(cookies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("There is no 'email' key");
+            }
+
+            Console.WriteLine($"Size: {cookies.Count}");
+
+            Console.WriteLine("ALL COOKIES: ");
+
+            foreach (KeyValuePair<string, string> item in cookies)//KeyValue pode ser alterado por "var"
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+    }
+}
